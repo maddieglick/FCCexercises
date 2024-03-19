7ddca35d7abd5ce949a065f1075ea72cc2dd0fbb
maddieglick <100810797+maddieglick@users.noreply.github.com>
2024-03-18 20:15:13 -0400

    Create dna-split.js
    work in progress

---
+++ dna-split.js
@@ -0,0 +1,19 @@
+function pairElement(str) {
+//maybe split and concat?
+//C to be followed by G
+//G to be followed by C
+//A to be followed by T
+//T to be followed by A
+
+//have to check if the letter's pair exists before the letter, after, or not at all omg
+
+//split into array of letters
+const letters = str.split('');
+
+//
+for (let i in letters){
+
+}
+}
+
+pairElement("GCG");

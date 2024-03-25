e3be2c9ccced9e5bdfec4f9d1546318974a48a89
maddieglick <100810797+maddieglick@users.noreply.github.com>
2024-03-25 14:24:51 -0400

    Update convert-html-entities.js
    solved. 
    
    Could have used split, switch case, and join to do it, or a few other ways.

--- convert-html-entities.js
+++ convert-html-entities.js
@@ -1,36 +1,22 @@
 function convertHTML(str) {
- //loop through the string
- //if charcodeat(i) == one of the chars
- //replace with corresponding html entity
- //need to make sure it doesnt stop after the first one
- //if no html entities, return string
-let strToArr = str.split('')
-let htmlEnt = ''
-for(let i=0; i<str.length; i++){
-  switch(str.charCodeAt(i)){
-    case 38:
-    htmlEnt = '&amp;'
-    break
-    case 60: 
-    htmlEnt = '&lt;'
-    break
-    case 62: 
-    htmlEnt = '&gt;'
-    break
-    case 34:
-    htmlEnt = '&quot;'
-    break
-    case 39:
-    htmlEnt = '&apos;'
-    break
-    default:
-    htmlEnt = str
+
+let htmlEnt = str.slice(0,)
+for(let i=0; i<htmlEnt.length; i++){
+    if(str.charCodeAt(i) == 38){
+      htmlEnt = htmlEnt.replace(htmlEnt[i], '&amp;')
+    }else if(htmlEnt.charCodeAt(i) == 60){
+      htmlEnt = htmlEnt.replaceAll(htmlEnt[i], '&lt;')
+    }else if(htmlEnt.charCodeAt(i) == 34){
+      htmlEnt = htmlEnt.replaceAll(htmlEnt[i],'&quot;')
+    }else if(htmlEnt.charCodeAt(i) == 62){
+      htmlEnt = htmlEnt.replaceAll(htmlEnt[i], '&gt;')
+    }else if(htmlEnt.charCodeAt(i) == 39){
+      htmlEnt = htmlEnt.replace(htmlEnt[i], '&apos;')
     }
-    console.log(htmlEnt)
-  return htmlEnt;
   }
+  return htmlEnt;
 }
 
-convertHTML("Dolce & Gabbana");
+convertHTML("<>");
 
 

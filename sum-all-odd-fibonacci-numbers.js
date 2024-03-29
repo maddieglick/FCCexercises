b19dbf1eb7e64415ea9f4335277cbe8c8a7e770e
maddieglick <100810797+maddieglick@users.noreply.github.com>
2024-03-29 17:41:50 -0400

    Update sum-all-odd-fibonacci-numbers.js
    simplified code! i always forget you can do method.().method()

--- sum-all-odd-fibonacci-numbers.js
+++ sum-all-odd-fibonacci-numbers.js
@@ -1,16 +1,12 @@
 function sumFibs(num) {
-  let fib = [0, 1];
+  let fib = [0, 1]
   for(let i = 0; fib[i] < num; i++){ //fib sequence into an array
-  if( fib[i] + fib[i+1] <= num){
+  if( fib[i] + fib[i+1] <= num){ //stop pushing when sum is <= num
     fib.push(fib[i] + fib[i+1])
     }
-  }console.log(fib)
-  let oddFibs = fib.filter((num) => num%2 == 1)
-  let sumOddFibs = oddFibs.reduce((start, next) => start+next)
-  return sumOddFibs
-  //filter to grab only odd numbers
-  //map to add all odd numbers together
-  //return sum of odd fibonacci numbers
+  }
+  let oddFibSum = fib.filter((num) => num%2 == 1).reduce((start, next) => start+next) //remove even nums, sum remaining nums
+  return oddFibSum
 
 }
 

7fa004f4032e01b75913832f2a212dde908b80e5
maddieglick <100810797+maddieglick@users.noreply.github.com>
2024-04-02 22:18:53 +0000

    Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.

---
+++ .vscode/sum-all-primes.js
@@ -0,0 +1,19 @@
+let sumPrimes = (num) => {
+    let sieve = [], primes = [];
+      for (let i = 2; i <= num; ++i) {
+          if (!sieve[i]) {
+              // i has not been marked -- it is prime
+              primes.push(i);
+              for (let j = i << 1; j <= num; j += i) {
+                  sieve[j] = true;
+              }
+          }
+      }
+      let primeSum = primes.reduce((val1, val2) => val1 + val2 )
+      return primeSum;
+  }
+     
+  //loop through nums until num
+  //for each number, if it is prime, add to sum
+  
+  sumPrimes(10);
